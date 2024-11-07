82be7ee17777a79c588e4db9bc313aff76e29993
merveozer <ozeermerve@gmail.com>
2024-11-07 17:50:56 +0300

    Fix: Microservice structure is removed, a unit test is corrected

--- order-service/src/main/java/com/ozer/microservices/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/ozer/microservices/order_service/client/InventoryClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+@FeignClient(value = "inventory", url = "http://localhost:5057")
 public interface InventoryClient {
     Logger log = LoggerFactory.getLogger(InventoryClient.class);
 //    @GetExchange("/api/inventory")
