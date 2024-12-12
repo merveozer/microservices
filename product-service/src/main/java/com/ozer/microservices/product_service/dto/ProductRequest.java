9107533a11cfd2f0790593898bb95a99b958e935
merveozer <ozeermerve@gmail.com>
2024-12-12 13:54:07 +0300

    Feature: ProductImportJob was implemented

--- product-service/src/main/java/com/ozer/microservices/product_service/dto/ProductRequest.java
+++ product-service/src/main/java/com/ozer/microservices/product_service/dto/ProductRequest.java
@@ -1,6 +1,9 @@
 package com.ozer.microservices.product_service.dto;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 
+@Builder
 public record ProductRequest(String id, String name, String description, BigDecimal price) {
 }
